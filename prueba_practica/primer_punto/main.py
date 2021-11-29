47e5dae714d8941d8f1829165548064aaf95bf38
Victor Castañeda <vidanilo10@gmail.com>
2021-11-29 14:25:30 -0500

    First one and gitignore

---
+++ prueba_practica/primer_punto/main.py
@@ -0,0 +1,28 @@
+import argparse
+import sys
+import os
+
+
+def process_file(name: str) -> str:
+    with open(name, newline='', mode='r') as input:
+        with open ('output.csv', mode='w') as output:
+            for line in input: 
+                int_list = [int(x) for x in line.split()]            
+                if sum(int_list) % 3:
+                    output.write('NO\n')
+                else:
+                    output.write('SI\n')
+
+def main() -> bool:
+    try:
+        os.remove("output.csv") 
+    except:
+        raise ValueError('File no exists')
+    finally:
+        parser = argparse.ArgumentParser(prog='MAIN')
+        parser.add_argument('--filename', type=str, default='input.csv')
+        args = parser.parse_args()
+        return process_file(args.filename)
+    
+if __name__ == '__main__':
+    sys.exit(main())
