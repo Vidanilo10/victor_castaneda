c39178c56c387acd9b4a80006465f5e735566f25
Victor Castañeda <vidanilo10@gmail.com>
2021-11-29 15:44:17 -0500

    Add models to app

--- prueba_practica/segundo_punto/facturas_electronicas/models.py
+++ prueba_practica/segundo_punto/facturas_electronicas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Factura(models.Model):
+    identificador = models.IntegerField('Identificador')
+    NIT = models.IntegerField('NIT')
+    descripccion = models.TextField('Descripcción')
+    valor_total = models.PositiveIntegerField('Valor total')
+    porcentaje_iva = models.PositiveIntegerField('Porcentaje de IVA')
+
+    def __str__(self):
+        return f'{self.identificador}, {self.descripccion}, {self.valor_total}'
