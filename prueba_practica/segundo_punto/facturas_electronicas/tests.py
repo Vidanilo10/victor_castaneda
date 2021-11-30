29f7a0604b28672f63c0d86f08106e38186b23bb
Victor Castañeda <vidanilo10@gmail.com>
2021-11-29 20:30:26 -0500

    Test model

--- prueba_practica/segundo_punto/facturas_electronicas/tests.py
+++ prueba_practica/segundo_punto/facturas_electronicas/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from models import Factura
 
-# Create your tests here.
+class ModelTest(TestCase):
+
+    def test_factura(self):
+        identificador: int = 12334
+        NIT: int = 83883
+        descripccion: str = 'Factura de prueba'
+        valor_total: int = 22222
+        porcentaje_iva: int = 333
+        
+        factura = Factura.objects.create_user(
+            identificador = identificador,
+            NIT = NIT,
+            descripccion = descripccion,
+            valor_total = valor_total,
+            porcentaje_iva = porcentaje_iva
+        )
+
+        self.assertEqual(factura.identificador, identificador)
+        self.assertEqual(factura.NIT, NIT)
+
+        
